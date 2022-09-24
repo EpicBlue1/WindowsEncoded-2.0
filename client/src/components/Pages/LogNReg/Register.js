041b1404feb8719416b60372d56415f98fa5ffc0
EpicBlue1 <21100115@virtualwindow.co.za>
2022-09-24 14:11:09 +0200

    Register 90% done
    have to add profile section
    
    Description added
    Placeholders added
    Layout Tweaked

--- client/src/components/Pages/LogNReg/Register.js
+++ client/src/components/Pages/LogNReg/Register.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Style from './LogNReg.module.scss';
 import Input from '../../subcomponents/Inputs/Input';
 import Button from '../../subcomponents/Buttons/Button';
 
 const Register = (props) => {
+
     return (
-        <div className={props.changeCards ? Style.SignupBack : Style.Signup}>
-            <Input Intype='Login'/>
-            <Input Intype='Login'/>
-            <Input type='number' Intype='Login'/>
-            <Input Intype='Login'/>
-            <Button type='Primary' onClick={() => {props.setChangeCard(!props.changeCards)}}>Log In Rather?</Button>
-        </div>
+        <>
+            <div className={props.changeCards ? Style.SignupBack : Style.Signup}>
+            <h1 className={Style.Spacing}>Register!</h1>
+                <Input placeholder='Username' Intype='Login'/>
+                <Input placeholder='Email' Intype='Login'/>
+                <Input placeholder='Password' Intype='Login'/>
+                <Input  className={Style.Spacing} placeholder='Confirm Password' Intype='Login'/>
+                <Button type='Primary'>Log In Rather?</Button>
+                <p onClick={() => {props.setChangeCard(!props.changeCards)}}>Already have an account? Login now!</p>
+            </div>
+
+            <div className={props.changeCards ? Style.SignupTextBack : Style.SignupText}>
+                <h2>About WindowsEncoded</h2>
+                <p>Aut inventore consequuntur et rerum doloremque.</p>
+                <ul>
+                    <li>Ask Questions</li>
+                    <li>Get Answers</li>
+                    <li>Answers Questions</li>
+                    <li>Profile Progression</li>
+                    <li>Earn badges</li>
+                </ul>
+                <p>Completely anonymous!</p>
+                <p>Only Requirement is an OW account</p>
+            </div>
+        </>
+
     );
 };
 
