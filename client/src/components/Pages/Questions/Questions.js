f2744bd82892286ff236ca11391f8b0257f9ef8b
200089_Bee! <200089@virtualwindow.co.za>
2022-09-27 14:35:06 +0200

    Question Page Complete + Question Card Styled

--- client/src/components/Pages/Questions/Questions.js
+++ client/src/components/Pages/Questions/Questions.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Style from './Questions.module.scss';
 import TopNav from '../../subcomponents/TopNav/TopNav';
 import LeftNav from '../../subcomponents/LeftNav/LeftNav';
-import Button from '../../subcomponents/Buttons/Button';
+import QuestionCard from '../../Cards/QuestionCard/QuestionCard';
 
 const Questions = () => {
     return (
@@ -13,6 +13,8 @@ const Questions = () => {
             <div className={Style.questionBlock}>
                 <div className={Style.addButton}>+</div>
                 <h2 className={Style.heading}>ASK A QUESTION</h2>
+
+                <QuestionCard/>
             </div>
         </div>
     );
