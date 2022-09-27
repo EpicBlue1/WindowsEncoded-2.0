3b93988f9aad519209293d375933b092602d38cd
T2sha <21100474@virtualwindow.co.za>
2022-09-27 18:15:55 +0200

    Added introduction

--- client/src/components/Pages/Landing/Landing.js
+++ client/src/components/Pages/Landing/Landing.js
@@ -4,15 +4,28 @@ import LeftNav from '../../subcomponents/LeftNav/LeftNav';
 import TopNav from '../../subcomponents/TopNav/TopNav';
 
 
+
 const Landing = () => {
     return (
+        <>
     
       <div className={Style.body}>
       <TopNav/>
-      <LeftNav/>
-            
-        </div>
+      <LeftNav/>    
+      </div>
+
+      <div className={Style.LandingImg}></div>
+         <h2 className={Style.heading}>HELLO, USER!</h2>
+         <p className={Style.text}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to...s</p>
+
+      
+
+      
+
+    </>
     );
+  
 };
+
 
 export default Landing;
---
+++ client/src/components/subcomponents/LandingCards/LandingCards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Style from './LandingCards.module.scss';
+
+const LandingCards = ()=>{
+    return(
+        <div>
+            <Card/>
+
+        </div>
+
+    );
+}
+
+export default LandingCards
