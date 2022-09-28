818027e35a44c176304404021b9271c21c18cbae
Justin-OwiStudent <200100@virtualwindow.co.za>
2022-09-28 13:53:09 +0200

    search.svg fault fixed

--- client/src/components/subcomponents/LeftNav/LeftNav.js
+++ client/src/components/subcomponents/LeftNav/LeftNav.js
@@ -1,50 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import CheckBox from '../CheckBox/CheckBox';
-import Style from './LeftNav.module.scss';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import CheckBox from "../CheckBox/CheckBox";
+import Style from "./LeftNav.module.scss";
 
 const LeftNav = () => {
-    return (
-        <div className={Style.leftNav}>
-            <Link to="/Landing">
-            <div className={Style.Home}>
-                <div className={Style.IconSpot}>
-                    <div className={Style.Icon}></div>
-                </div>
-                <h3 className={Style.text}>Home</h3>
-            </div>
-            </Link>
-           
-            <Link to="/Questions">
-            <div className={Style.Questions}>
-                <div className={Style.IconSpot}>
-                    <div className={Style.Icon2}></div>                   
-                </div>
-                <h3 className={Style.text}>Questions</h3>
-            </div>
-            </Link>
-           
-
-            <div className={Style.Topics}>
-                <h2>Topics</h2>
-
-                <CheckBox/>
-                <CheckBox/>
-                <CheckBox/>
-                <CheckBox/>
-
-                {/* <input id='1' type="checkbox" className={Style.CheckBox}/>
-                <label for="1" className={Style.label}> Css </label> */}
-            </div>
-
-            <div className={Style.Logout}>
-                <div className={Style.LogImg}></div>
-                <h2 className={Style.LogoutText}>Logout</h2>
-            </div>
-
+  return (
+    <div className={Style.leftNav}>
+      <Link to="/Landing">
+        <div className={Style.Home}>
+          <div className={Style.IconSpot}>
+            <div className={Style.Icon}></div>
+          </div>
+          <h3 className={Style.text}>Home</h3>
         </div>
-    );
+      </Link>
+
+      <Link to="/Questions">
+        <div className={Style.Questions}>
+          <div className={Style.IconSpot}>
+            <div className={Style.Icon2}></div>
+          </div>
+          <h3 className={Style.text}>Questions</h3>
+        </div>
+      </Link>
+
+      <div className={Style.Topics}>
+        <h2>Topics</h2>
+
+        <CheckBox />
+        <CheckBox />
+        <CheckBox />
+        <CheckBox />
+
+       
+      </div>
+
+      <Link to="/">
+        <div className={Style.Logout}>
+          <div className={Style.LogImg}></div>
+          <h2 className={Style.LogoutText}>Logout</h2>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default LeftNav;
