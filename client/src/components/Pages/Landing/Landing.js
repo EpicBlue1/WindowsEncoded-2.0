771c14944943601bd876381bb56859bce9f13976
EpicBlue1 <21100115@virtualwindow.co.za>
2022-09-28 11:30:32 +0200

    Landing Page Done For class

--- client/src/components/Pages/Landing/Landing.js
+++ client/src/components/Pages/Landing/Landing.js
@@ -13,9 +13,39 @@ const Landing = () => {
 
         <div className={Style.LandingArea}>
             <div className={Style.Block}></div>
-            <h2 className={Style.heading}>HELLO, USER!</h2>
-            <p className={Style.Paragraph}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to...s</p>
+            <div className={Style.Content}>
+                <h2 className={Style.heading}>HELLO, USER!</h2>
+                <p className={Style.Paragraph}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to...s</p>
+            </div>
         </div>
+
+        <h1 className={Style.Heading}>About Us</h1>
+
+        <div className={Style.Row}>
+            <div className={Style.Container}></div>
+            <div className={Style.Container}></div>
+            <div className={Style.Container}></div>
+        </div>
+
+        <h1 className={Style.Heading}>CATEGORIES/TAGS</h1>
+
+        <div className={Style.Row}>
+            <div className={Style.Container}>
+                <div className={Style.ImageOne}></div>
+                <h2>HTML</h2>
+            </div>
+            <div className={Style.Container}>
+                <div className={Style.ImageTwo}></div>
+                <h2>Javascript</h2>
+            </div>
+            <div className={Style.Container}>
+                <div className={Style.ImageThree}></div>
+                <h2>React</h2>
+            </div>
+        </div>
+
+
+
     </div>
     );
   
