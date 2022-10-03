91191c9896cfb61dd5df471dfcc3d729b77cba55
200089_Bee! <200089@virtualwindow.co.za>
2022-10-03 16:59:28 +0200

    QuestionSchema

--- server/models/Questions.js
+++ server/models/Questions.js
@@ -1,5 +1,70 @@
 const mongoose = require('mongoose');
 
 const QuestionSchema = mongoose.Schema({
-    
+    userId: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    questionImage: {
+        type: String,
+        required: true
+    },
+    questionTitle: {
+        type: String,
+        required: true
+    },
+    questionDescription: {
+        type: String,
+        required: true
+    },
+    codeSnippet: {
+        type: String,
+        required: true
+    },
+    tags: {
+        type: Array,
+        required: true
+    },
+    upvotes: {
+        type: Number,
+        required: true
+    },
+    downvotes: {
+        type: Number,
+        required: true
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    Answers: {
+        userId: {
+            type: String,
+            required: true
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        Answer: {
+            type: String,
+            required: true
+        },
+        upvotes: {
+            type: Number,
+            required: true
+        },
+        downvotes: {
+            type: Number,
+            required: true
+        },
+        score: {
+            type: Number,
+            required: true
+        },
+    }
 })
