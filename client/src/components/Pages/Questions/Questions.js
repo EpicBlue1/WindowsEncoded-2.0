7385e99e816b8725d462b379961e27f0d99a703c
200089_Bee! <200089@virtualwindow.co.za>
2022-10-03 16:01:45 +0200

    Updated styling - add question modal/modal input

--- client/src/components/Pages/Questions/Questions.js
+++ client/src/components/Pages/Questions/Questions.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Style from './Questions.module.scss';
 import QuestionCard from '../../Cards/QuestionCard/QuestionCard';
 import AddQuestion from '../../subcomponents/AddQuestion/AddQuestion';
 
 const Questions = () => {
+
+    const [addQuestionModal, setAddQuestionModal] = useState();
+
+    const addQuestion = () => {
+        setAddQuestionModal(<AddQuestion rerender={setAddQuestionModal}/>)
+    }
+
     return (
         <div className={Style.body}>
 
-            <AddQuestion/>
+            {addQuestionModal}
 
-            <div className={Style.addButton}>+</div>
+            <div className={Style.addButton} onClick={addQuestion}>+</div>
             <h2 className={Style.heading}>ASK A QUESTION</h2>
 
             <QuestionCard/>
--- client/src/components/subcomponents/AddQuestion/AddQuestion.js
+++ client/src/components/subcomponents/AddQuestion/AddQuestion.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Style from './AddQuestion.module.scss';
 import AddImageIcon from '../../../Icons/user-plus.svg';
 import Input from '../Inputs/Input';
-import TextArea from '../TextArea/TextArea';
+import Button from '../../subcomponents/Buttons/Button';
 
-const AddQuestion = () => {
+const AddQuestion = (props) => {
+
+    const [questionInputs, setQuestionInputs] = useState();
+
+    const closeModal = () => {
+        props.rerender()
+    }
+
+    const questionInfo = (e) => {
+        const {name, value} = e.target;
+        setQuestionInputs({...questionInputs, [name]: value});
+    }
+
+    const addQuestion = () => {
+        console.log(questionInputs);
+    }
+
     return (
-        <div className={Style.addQuestionCard}>
+        <div className={Style.BackgroundBlur}>
+            <div className={Style.addQuestionCard}>
 
-            <div className={Style.closeButton}>
-                <div>x</div>
+                <div className={Style.closeButton} onClick={closeModal}>
+                    <div>x</div>
+                </div>
+
+                <h2>Add a Question</h2>
+
+                <Input Intype="ModalInput" placeholder="Title..." name="questionTitle" onChange={questionInfo}/>
+
+
+                <div className={Style.Preview} style={{backgroundImage: `url(${AddImageIcon})`}}></div> 
+
+                <textarea className={Style.textBox} name="questionDescription" onChange={questionInfo}></textarea>
+                <textarea className={Style.codeBox} name="codeSnippet" onChange={questionInfo}></textarea>
+
+                <Button type="Primary" onClick={addQuestion}>Add Question</Button>
             </div>
-
-            <h2>Add a Question</h2>
-
-            <Input Intype="Login" placeholder="Title..."/>
-
-
-            <div className={Style.Preview} style={{backgroundImage: `url(${AddImageIcon})`}}></div>
-            <textarea className={Style.textBox}></textarea>
-            
         </div>
     );
 };
