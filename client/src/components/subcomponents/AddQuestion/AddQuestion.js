71564d0447fadd67c78462d44b1d920fc3760733
200089_Bee! <200089@virtualwindow.co.za>
2022-10-03 09:56:36 +0200

    Update AddQuestion.js

--- client/src/components/subcomponents/AddQuestion/AddQuestion.js
+++ client/src/components/subcomponents/AddQuestion/AddQuestion.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Style from './AddQuestion.module.scss';
+import Input from '../Inputs/Input';
 
 const AddQuestion = () => {
     return (
         <div className={Style.addQuestionCard}>
             <div className={Style.close}></div>
+
+            <Input Intype="Login"/>
             
         </div>
     );
