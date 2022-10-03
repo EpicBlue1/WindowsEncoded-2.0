6bcfa7176f819b6a59c64303f50e49f4b0327352
200089_Bee! <200089@virtualwindow.co.za>
2022-10-03 17:22:58 +0200

    Updated input with name

--- client/src/components/subcomponents/Inputs/Input.js
+++ client/src/components/subcomponents/Inputs/Input.js
@@ -5,7 +5,7 @@ const Input = (props) => {
     return (
 
     <div className={`${Style.InputWidth} ${props.className} `}>
-        <input ref={props.ref} onChange={props.onChange} required={props.required} type={props.type} placeholder={props.placeholder} className={`${props.className ? props.className : ""} ${props.Intype === "Search" ? Style.Search : props.Intype === "Login" ? Style.Login : props.Intype === "ModalInput" ? Style.ModalInput : ""}`}/>
+        <input name={props.name} ref={props.ref} onChange={props.onChange} required={props.required} type={props.type} placeholder={props.placeholder} className={`${props.className ? props.className : ""} ${props.Intype === "Search" ? Style.Search : props.Intype === "Login" ? Style.Login : props.Intype === "ModalInput" ? Style.ModalInput : ""}`}/>
         <div className={props.Intype === 'Search' ? Style.Icon : "hide"}></div>
     </div>
 
