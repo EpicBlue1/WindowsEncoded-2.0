9459c1637e9d56648d8ebbb5e6c253f59c7034e2
200089_Bee! <200089@virtualwindow.co.za>
2022-10-03 11:32:42 +0200

    close button

--- client/src/components/subcomponents/AddQuestion/AddQuestion.js
+++ client/src/components/subcomponents/AddQuestion/AddQuestion.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Style from './AddQuestion.module.scss';
+import AddImageIcon from '../../../Icons/user-plus.svg';
 import Input from '../Inputs/Input';
+import TextArea from '../TextArea/TextArea';
 
 const AddQuestion = () => {
     return (
         <div className={Style.addQuestionCard}>
-            <div className={Style.close}></div>
+
+            <div className={Style.closeButton}>
+                <div>x</div>
+            </div>
+
+            <h2>Add a Question</h2>
 
             <Input Intype="Login" placeholder="Title..."/>
+
+
+            <div className={Style.Preview} style={{backgroundImage: `url(${AddImageIcon})`}}></div>
+            <textarea className={Style.textBox}></textarea>
             
         </div>
     );
