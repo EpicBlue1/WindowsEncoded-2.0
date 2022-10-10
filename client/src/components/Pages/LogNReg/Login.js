d702a3b4d660a18bebaece186dcca7db850a6641
EpicBlue1 <21100115@virtualwindow.co.za>
2022-10-10 11:28:22 +0200

    Session Storage Stores Data

--- client/src/components/Pages/LogNReg/Login.js
+++ client/src/components/Pages/LogNReg/Login.js
@@ -1,131 +1,180 @@
-import React, { useRef } from 'react';
-import Style from './LogNReg.module.scss';
-import Input from '../../subcomponents/Inputs/Input';
-import axios from 'axios';
-import Button from '../../subcomponents/Buttons/Button';
-import ForgetPassword from '../../subcomponents/ForgetPassword/ForgetPassword';
-import {useState} from 'react';
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../subcomponents/Buttons/Button";
+import ForgetPassword from "../../subcomponents/ForgetPassword/ForgetPassword";
+import Input from "../../subcomponents/Inputs/Input";
+import Style from "./LogNReg.module.scss";
 
 const Login = (props) => {
+  const email = useRef(),
+    password = useRef(),
+    Navigate = useNavigate();
 
-    const email = useRef(),
-    password = useRef()
+  const [forgotPassword, setForgotPassword] = useState(false);
 
-    const [forgotPassword, setForgotPassword] = useState(false)
+  const [CredentialsVal, setCredentialsVal] = useState(""),
+    [emailValid, setEmailValid] = useState(""),
+    [passwordValid, setPasswordValid] = useState("");
 
-    const [CredentialsVal, setCredentialsVal] = useState(''),
-    [emailValid, setEmailValid] = useState(''),
-    [passwordValid, setPasswordValid] = useState('')
-
-    const [EmailText, setEmailText] = useState(),
+  const [EmailText, setEmailText] = useState(),
     [PasswordText, setPasswordText] = useState(),
-    [FormText, setFormText] = useState()
+    [FormText, setFormText] = useState();
 
+  //Validation
+  const FormValues = () => {
+    //Email
+    let Emailregex = new RegExp("[0-9]+@virtualwindow.co.za");
+    if (!Emailregex.test(email.current.value)) {
+      setEmailValid(false);
+      setEmailText(
+        "Please enter your student email (Student number + virtualwindow.co.za)"
+      );
+    } else {
+      setEmailValid(true);
+    }
 
-    //Validation
-    const FormValues = () => {
-        //Email
-        let Emailregex = new RegExp('[0-9]+@virtualwindow.co.za');
-        if(!Emailregex.test(email.current.value)){
-            setEmailValid(false);
-            setEmailText('Please enter your student email (Student number + virtualwindow.co.za)');
-        } else {
+    if (email.current.value === "") {
+      setEmailValid("");
+    }
+
+    //Password
+    if (
+      password.current.value.length !== 1 &&
+      password.current.value.length <= 1
+    ) {
+      setPasswordValid(false);
+      setPasswordText("Please enter Password");
+    } else {
+      setPasswordValid(true);
+      setPasswordText("All good");
+    }
+
+    if (password.current.value === "") {
+      setPasswordValid("");
+    }
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    // const newProduct = new FormData()
+    if (password.current.value !== "" && password.current.value !== "") {
+      let userCreds = {
+        email: email.current.value,
+        password: password.current.value,
+      };
+
+      axios
+        .post("http://localhost:2000/login", userCreds)
+        .then((res) => {
+          console.log(res.data.Message);
+          console.log(res);
+          if (res.data.Message === "Valid") {
+            setFormText("");
             setEmailValid(true);
-        }
-
-        if(email.current.value === ''){
-            setEmailValid('');
-        }
-
-        //Password
-        if(password.current.value.length !== 1 && password.current.value.length <= 1){
-            setPasswordValid(false);
-            setPasswordText('Please enter Password');
-        } else {
             setPasswordValid(true);
-            setPasswordText('All good');
-        }
-
-        if(password.current.value === ''){
-            setPasswordValid('');
-        }
-    }
-
-    const login = (e) => {
-        e.preventDefault();
-        // const newProduct = new FormData()
-        if(password.current.value !== '' && password.current.value !== ''){
-            let userCreds = {
-                email: email.current.value,
-                password: password.current.value,
-            }
-    
-            axios.post('http://localhost:2000/login', userCreds)
-            .then((res) => {
-                console.log(res.data.Message);
-                console.log(res);
-                    if(res.data.Message === 'Valid'){
-                        setFormText("")
-                        setEmailValid(true);
-                        setPasswordValid(true);
-                        setPasswordText('All Good');
-                        setEmailText('All Good');
-                    } else {
-                        setFormText("Password or Email doesn't match")
-                        setEmailValid(false);
-                        setPasswordValid(false);
-                        setPasswordText('Double Check Password');
-                        setEmailText('Double Check Email');
-                    }
-                // form.current.reset();
-                // setRender(prev => !prev)
-            }).catch((err) => {
-                console.log(err)
-            })
-            // props.setShow(false);
-        } else {
-            setFormText("Please add your details");
-            setEmailText('Please enter your student email (Student number + virtualwindow.co.za)');
-            setPasswordText('Please enter Password');
+            setPasswordText("All Good");
+            setEmailText("All Good");
+            // Navigate("/");
+            sessionStorage.setItem("UserData", JSON.stringify(res.data.user));
+          } else {
+            setFormText("Password or Email doesn't match");
+            setEmailValid(false);
             setPasswordValid(false);
-            setEmailValid(false)
-        }
+            setPasswordText("Double Check Password");
+            setEmailText("Double Check Email");
+          }
+          // form.current.reset();
+          // setRender(prev => !prev)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      // props.setShow(false);
+    } else {
+      setFormText("Please add your details");
+      setEmailText(
+        "Please enter your student email (Student number + virtualwindow.co.za)"
+      );
+      setPasswordText("Please enter Password");
+      setPasswordValid(false);
+      setEmailValid(false);
     }
+  };
 
-    return (
-        <>
-        <div className={props.changeCards ? Style.Login : Style.LoginBack}>
-            <div className={Style.closeButton}>
-                <Link to="/">
-                    <div className={Style.White} onClick={() => {props.setShowNav(true)}}>x</div>
-                </Link>
+  return (
+    <>
+      <div className={props.changeCards ? Style.Login : Style.LoginBack}>
+        <div className={Style.closeButton}>
+          <Link to="/">
+            <div
+              className={Style.White}
+              onClick={() => {
+                props.setShowNav(true);
+              }}
+            >
+              x
             </div>
-        <ForgetPassword forgotPassword={forgotPassword} setForgotPassword={setForgotPassword}/>
-            <div className={Style.LogLeft}>
-                <h1 className={Style.Spacing}>Welcome back!</h1>
-            <br></br>
-                <form onChange={FormValues}>
-                    <Input text={EmailText} Valid={emailValid} ref={email} placeholder='Email' className={Style.margin} Intype='Login'/>
-
-                    <Input text={PasswordText} Valid={passwordValid} ref={password} placeholder='Password' className={Style.margin} Intype='Login'/>
-                </form>
-            <br></br>
-                <p className={Style.Red}>{FormText}</p>
-                <p onClick ={()=> {setForgotPassword(true)}}>Forgot Password?</p>
-            
-                <p className={Style.Spacing} onClick={() => {props.setChangeCard(!props.changeCards)}}>Don't have an account? Register now!</p>
-
-                <Button onClick={login} className={Style.Button} type='Primary'>Log In</Button>
-            </div>
-            <div className={Style.LogRight}>
-                <div className={Style.LogImage}></div>
-                <a href="https://www.freepik.com/free-vector/hand-coding-concept-illustration_21864184.htm#query=coding&position=17&from_view=search">Image by storyset on Freepik</a> 
-            </div>
-
+          </Link>
         </div>
-        </>
-    );
+        <ForgetPassword
+          forgotPassword={forgotPassword}
+          setForgotPassword={setForgotPassword}
+        />
+        <div className={Style.LogLeft}>
+          <h1 className={Style.Spacing}>Welcome back!</h1>
+          <br></br>
+          <form onChange={FormValues}>
+            <Input
+              text={EmailText}
+              Valid={emailValid}
+              ref={email}
+              placeholder="Email"
+              className={Style.margin}
+              Intype="Login"
+            />
+
+            <Input
+              text={PasswordText}
+              Valid={passwordValid}
+              ref={password}
+              placeholder="Password"
+              className={Style.margin}
+              Intype="Login"
+            />
+          </form>
+          <br></br>
+          <p className={Style.Red}>{FormText}</p>
+          <p
+            onClick={() => {
+              setForgotPassword(true);
+            }}
+          >
+            Forgot Password?
+          </p>
+
+          <p
+            className={Style.Spacing}
+            onClick={() => {
+              props.setChangeCard(!props.changeCards);
+            }}
+          >
+            Don't have an account? Register now!
+          </p>
+
+          <Button onClick={login} className={Style.Button} type="Primary">
+            Log In
+          </Button>
+        </div>
+        <div className={Style.LogRight}>
+          <div className={Style.LogImage}></div>
+          <a href="https://www.freepik.com/free-vector/hand-coding-concept-illustration_21864184.htm#query=coding&position=17&from_view=search">
+            Image by storyset on Freepik
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- server/routes.js
+++ server/routes.js
@@ -1,58 +1,63 @@
-const express = require('express');
+const express = require("express");
 const router = express();
-const path = require('path');
-const bcrypt = require('bcrypt');
-const usersSchema = require('./models/Users');
-const multer = require('multer');
-const questionModel = require('./models/Questions');
+const path = require("path");
+const bcrypt = require("bcrypt");
+const usersSchema = require("./models/Users");
+const multer = require("multer");
+const questionModel = require("./models/Questions");
 
 // multer middleware
 //Question Images
 const storedQuestionImage = multer.diskStorage({
-    destination: (req, file, callBack) => {
-        callBack(null, './QuestionImages');
-    },
+  destination: (req, file, callBack) => {
+    callBack(null, "./QuestionImages");
+  },
 
-    filename: (req, file, callBack) => {
-        callBack(null, Date.now() + path.extname(file.originalname))
-    }
+  filename: (req, file, callBack) => {
+    callBack(null, Date.now() + path.extname(file.originalname));
+  },
 });
 
-const uploadQuestionImage = multer({storage: storedQuestionImage});
+const uploadQuestionImage = multer({ storage: storedQuestionImage });
 
 //add question
-router.post('/api/newQuestion', uploadQuestionImage.single('image'), (req, res) => {
-
+router.post(
+  "/api/newQuestion",
+  uploadQuestionImage.single("image"),
+  (req, res) => {
     let data = JSON.parse(req.body.information);
     console.log(req.file.filename);
 
     const newQuestion = new questionModel({
-        questionTitle: data.questionTitle,
-        questionDescription: data.questionDescription,
-        codeSnippet:data.codeSnippet,
-        date: data.date,
-        image: req.file.filename
-    })
+      questionTitle: data.questionTitle,
+      questionDescription: data.questionDescription,
+      codeSnippet: data.codeSnippet,
+      date: data.date,
+      image: req.file.filename,
+    });
 
-    newQuestion.save()
-    .then(item => {
+    newQuestion
+      .save()
+      .then((item) => {
         res.json(item);
-    })
-    .catch(err => {n
-        res.status(400).json({msg: "There is an Error:", err})
-    })
-});
+      })
+      .catch((err) => {
+        n;
+        res.status(400).json({ msg: "There is an Error:", err });
+      });
+  }
+);
 
 // get all questions
-router.get('/api/allQuestions', async (req, res) => {
-    const findQuestions = await questionModel.find();
-    res.json(findQuestions);
+router.get("/api/allQuestions", async (req, res) => {
+  const findQuestions = await questionModel.find();
+  res.json(findQuestions);
 });
 
 // get one question
-router.get('/api/oneQuestion/:id', async (req, res) => {
-    const findQuestion = await questionModel.findById(req.params.id);
-    res.json(findQuestion);
+router.get("/api/oneQuestion/:id", async (req, res) => {
+  const findQuestion = await questionModel.findById(req.params.id);
+  res.json(findQuestion);
 });
 
 //add Profile Images
@@ -78,62 +83,56 @@ router.get('/api/oneQuestion/:id', async (req, res) => {
 //     });
 // });
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
+  const newUser = new usersSchema({
+    email: req.body.email,
+    username: req.body.username,
+    accStatus: false,
+    password: req.body.password,
+    profile: "profile",
+    score: 50,
+    admin: false,
+  });
 
-    const newUser = new usersSchema(
-        {
-            email: req.body.email,
-            username: req.body.username,
-            accStatus: false,
-            password: req.body.password,
-            profile: "profile",
-            score: 50,
-            admin: false,
-        }
-    );
-
-    newUser.save()
-    .then(item => {
-        res.json(item);
+  newUser
+    .save()
+    .then((item) => {
+      res.json(item);
     })
-    .catch(err => {
-        res.status(400).send(err.response);
-        console.log(err.response);
-        console.log(err.request);
-        console.log(err.message)
+    .catch((err) => {
+      res.status(400).send(err.response);
+      console.log(err.response);
+      console.log(err.request);
+      console.log(err.message);
     });
-})
+});
 
-router.post('/login', async (req, res) => {
-    
-    const findUser = await usersSchema.findOne({
-        email: req.body.email
-    });
+router.post("/login", async (req, res) => {
+  const findUser = await usersSchema.findOne({
+    email: req.body.email,
+  });
 
-    console.log(req.body);
+  console.log(req.body);
 
-    if(findUser){
-        const validPass = await bcrypt.compare(req.body.password, findUser.password);
-        if(validPass){
-            res.json(
-                {
-                    Message: "Valid",
-                }
-            );
-        } else {
-            res.json(
-                {
-                    Message: "InValid",
-                }
-            );
-        }
+  if (findUser) {
+    const validPass = await bcrypt.compare(
+      req.body.password,
+      findUser.password
+    );
+    if (validPass) {
+      res.json({
+        Message: "Valid",
+        user: findUser,
+      });
     } else {
-        res.json({user: false});
-        console.log("user doesn't exist")
+      res.json({
+        Message: "InValid",
+      });
     }
-
-})
-
-
+  } else {
+    res.json({ user: false });
+    console.log("user doesn't exist");
+  }
+});
 
 module.exports = router;
