1d835de22e7b4bf04f1ecb0724c1f405ab5db455
EpicBlue1 <21100115@virtualwindow.co.za>
2022-10-19 10:55:55 +0200

    Are you sure you dont want to log in

--- client/src/components/Pages/LogNReg/Confirmation.js
+++ client/src/components/Pages/LogNReg/Confirmation.js
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../subcomponents/Buttons/Button";
 import Style from "./LogNReg.module.scss";
 
 const Confirmation = (props) => {
-  //   const [Confirm, setConfirm] = useState(props.showConfirm);
+  const Navigate = useNavigate();
 
+  console.log(props.AreSure);
   return (
     <div
       ref={props.confirmDiv}
@@ -12,17 +14,35 @@ const Confirmation = (props) => {
         props.showConfirm ? Style.ConfirmationStart : Style.Confirmation
       }
     >
-      <h1>Your almost in!</h1>
-      <h3>Make sure you check your email before you continue to log in!</h3>
+      <h1>
+        {props.AreSure ===
+        "Are you sure? You wont be able to ask or answer questions."
+          ? ""
+          : "You are almost there"}
+        {/* Your almost in! */}
+      </h1>
+      <h3>{props.AreSure}</h3>
       <Button
         onClick={() => {
-          props.setShowConfirm(!props.showConfirm);
+          Navigate("/");
           console.log("Lol");
         }}
         type="Primary"
       >
         Ok
       </Button>
+      {props.AreSure ===
+      "Are you sure? You wont be able to ask or answer questions." ? (
+        <Button
+          onClick={() => {
+            props.setShowConfirm(!props.showConfirm);
+            console.log("Lol");
+          }}
+          type="Primary"
+        >
+          Cancel
+        </Button>
+      ) : null}
     </div>
   );
 };
