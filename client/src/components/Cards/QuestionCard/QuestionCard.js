f19e9085a814b765ae3e360a76daecefa6747794
EpicBlue1 <21100115@virtualwindow.co.za>
2022-10-22 23:27:48 +0200

    Busy with upvote logic

--- client/src/components/Cards/QuestionCard/QuestionCard.js
+++ client/src/components/Cards/QuestionCard/QuestionCard.js
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Style from "./QuestionCard.module.scss";
 
 const QuestionCard = (props) => {
   let navigate = useNavigate();
 
+  const [Voting, setVoting] = useState();
+
   const viewQuestion = () => {
     sessionStorage.setItem("questionId", props.productId);
     navigate("/IndividualQuestion", { state: { allData: props.allData } });
+  };
+
+  const UADVote = () => {
+    let upCount = 10;
+    let downCount = 10;
+
+    //convert 50 to 70
+    upCount = upCount * 1.4;
+
+    //convert 50 to 30
+    downCount = downCount * 0.6;
+
+    let total = upCount - downCount;
+
+    let downPercentage = 0;
+
+    //make sure the smallest number is divided
+    if (downCount < upCount) {
+      //percentage of downvotes to calculate percentage of upvotes
+      downPercentage = (downCount / upCount) * 100;
+    } else {
+      downPercentage = (upCount / downCount) * 100;
+    }
   };
 
   return (
@@ -40,7 +65,7 @@ const QuestionCard = (props) => {
           }}
         >
           <div className={Style.Upvote}></div>
-          <div className={Style.circle}>10</div>
+          <div className={Style.circle}>0</div>
           <div className={Style.Downvote}></div>
         </div>
       </div>
