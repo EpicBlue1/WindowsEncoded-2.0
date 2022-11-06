ddf5d9c2d899a5609d72ed92c2d06cf13d97edc0
Justin-OwiStudent <200100@virtualwindow.co.za>
2022-11-06 12:23:14 +0200

    fixed left nav sizing and logout moving

--- client/src/components/subcomponents/Admin/Admin.js
+++ client/src/components/subcomponents/Admin/Admin.js
@@ -8,30 +8,11 @@ const Admin = (props) => {
 
     const [Flagged, setFlagged] = useState();
 
-    // let renderQuestions = questionData.map((item) => (
-    //     <QuestionCard
-    //       key={item._id}
-    //       userId={item.userId}
-    //       username={item.username}
-    //       questionId={item._id}
-    //       questionTitle={item.questionTitle}
-    //       questionDescription={item.questionDescription}
-    //       codeSnippet={item.codeSnippet}
-    //       language={item.language}
-    //       image={URL + item.image}
-    //       editRender={setRenderQuestions}
-    //       allData={item}
-    //     />
-    //   ));
-
-    //   setQuestions(renderQuestions);
-    //   setRenderQuestions(false);
+ 
 
 
     useEffect(() => {
-        // const USER = sessionStorage.getItem("UserData");
-        // let user = JSON.parse(USER);
-        // let UserId = user._id;
+       
     
           axios.get("http://localhost:2000/api/allQuestions").then((res) => {
             let data = res.data;
@@ -52,37 +33,6 @@ const Admin = (props) => {
            });
 
            setFlagged(rendeFlagged)
-           
-
-
-            
-            // console.log(flagged);
-
-            
-
-            // if() {
-
-            // } else() {
-
-            // }
-            // let render = setProfileQuestions(
-            //   data
-            //     .filter((filterData) => UserId === filterData.userId)
-            //     // TODO: Rerender after editing
-            //     .map((Ques) => (
-            //       <ProfileQuestion
-            //         key={Ques._id}
-            //         alldata={Ques}
-            //         updateRender={updateRender}
-            //         setUpdateRender={setUpdateRender}
-            //       />
-            //     ))
-            // );
-            // let Qasked = data.filter(
-            //   (filterData) => seshStorage._id === filterData.userId
-            // );
-            // setAskedQuestions(Qasked.length);
-            // console.log(Qasked);
           });
         
       }, []);
